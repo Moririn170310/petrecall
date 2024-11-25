7b937f37d602e5a808366517749aebc8b65e4f35
Moririn170310 <230067ks@yse-c.net>
2024-11-25 16:15:56 +0900

    a

--- more.php
+++ more.php
@@ -19,5 +19,49 @@ $stmt = $pdo->query($sql);
 $pets = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
+<!DOCTYPE html>
+<html lang="ja">
 
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>登録された<?= $animal_data['name'] ?>の一覧</title>
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<!-- ヘッダー -->
+<header class="bg-white shadow">
+  <div class="max-w-7xl mx-auto px-4 py-6 flex justify-between items-center">
+    <nav>
+      <ul class="flex space-x-4">
+        <li><a href="../" class="text-gray-600 hover:text-blue-500">ホーム</a></li>
+        <li><a href="list.php" class="text-gray-600 hover:text-blue-500">ペット一覧</a></li>
+        <li><a href="../regist/" class="text-gray-600 hover:text-blue-500">ペット登録</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
+
+<body class="bg-gray-100">
+  <div class="container mx-auto p-8">
+    <?php if (isset($animal_data)): ?>
+      <h1 class="text-3xl font-bold mb-8 text-center">登録された<?= $animal_data['name'] ?>の一覧</h1>
+    <?php else: ?>
+      <h1 class="text-3xl font-bold mb-8 text-center">登録されたペットの一覧</h1>
+    <?php endif ?>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <?php foreach ($pets as $pet): ?>
+        <div class="bg-white p-4 rounded-lg shadow-lg">
+          <img src="../uploads/<?php echo htmlspecialchars($pet['image_name']); ?>" alt="画像" class="mt-2 w-full h-auto rounded-md">
+          <p href = "more.php" class="mt-2 text-gray-600">ペット名：<?php echo htmlspecialchars($pet['name']); ?></p>
+          <!-- <p class="mt-2 text-gray-600"><?php echo htmlspecialchars($pet['user_name']); ?>さんの犬</p> 
+                    <p class="mt-2 text-gray-600">Email: <?php echo htmlspecialchars($pet['email']); ?></p>
+                    <p class="mt-2 text-gray-600">住所: <?php echo htmlspecialchars($pet['address']); ?></p>
+                    <p class="mt-2 text-gray-600">電話番号: <?php echo htmlspecialchars($pet['phone']); ?></p>-->
+        </div>
+      <?php endforeach; ?>
+    </div>
+  </div>
+</body>
+
+</html>
 
